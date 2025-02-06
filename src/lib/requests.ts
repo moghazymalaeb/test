4acabe056d6d99952cbec9d0e583c069599f3506
Mahmoud Elmoghazy <152792227+moghazymalaeb@users.noreply.github.com>
2025-02-06 10:56:57 +0300

    Add double

--- src/lib/requests.ts
+++ src/lib/requests.ts
@@ -5,7 +5,7 @@ const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
   timeout: 10000, // 10 seconds timeout
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
 });
 
